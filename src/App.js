98f2ee1cef332922f6eb1ef89d427846becdb645
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-09-27 15:24:25 -0300

    Requisito 5 finalizado. H

--- src/App.js
+++ src/App.js
@@ -28,8 +28,37 @@ class App extends React.Component {
     });
   };
 
-  handleClickInButton = () => {
-
+  isSaveButtonDisabled = () => {
+    const {
+      nome,
+      description,
+      number1,
+      number2,
+      number3,
+      photo,
+      select,
+    } = this.state;
+    const uniqueAttr = 90;
+    const maxPower = 210;
+    const validateName = nome.length > 0;
+    const validateDescription = description.length > 0;
+    const validateImage = photo.length > 0;
+    const validateSelect = select.length > 0;
+    const maxSum = parseFloat(number1) + parseFloat(number2)
+      + parseFloat(number3) <= maxPower;
+    const validate1 = parseFloat(number1) >= 0 && parseFloat(number1) <= uniqueAttr;
+    const validate2 = parseFloat(number2) >= 0 && parseFloat(number2) <= uniqueAttr;
+    const validate3 = parseFloat(number3) >= 0 && parseFloat(number3) <= uniqueAttr;
+    return (
+      validateName
+      && validateDescription
+      && validateImage
+      && validateSelect
+      && validate1
+      && validate2
+      && validate3
+      && maxSum
+    );
   };
 
   render() {
@@ -56,9 +85,9 @@ class App extends React.Component {
           cardImage={ photo }
           cardRare={ select }
           cardTrunfo={ trunfo }
-          isSaveButtonDisabled=""
+          isSaveButtonDisabled={ this.isSaveButtonDisabled }
           onInputChange={ this.handleChange }
-          onSaveButtonClick={ this.handleClickInButton }
+          onSaveButtonClick=""
         />
         <Card
           cardName={ nome }
